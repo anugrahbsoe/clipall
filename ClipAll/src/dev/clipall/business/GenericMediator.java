058bb4b723dad5405844f430e64713296b631dc1
hira.erol <hira.erol@807286c6-d6b8-11de-b0e0-6bdc6d9bcfd0>
2010-01-10 17:08:25 +0000

    ClipAll 1.2 RC2 release - diffmerge, tortoise diff added - reverse paste added

--- ClipAll/src/dev/clipall/business/GenericMediator.java
+++ ClipAll/src/dev/clipall/business/GenericMediator.java
@@ -1,18 +1,17 @@
 /**
-* This file is part of dev.clipall.
-*
-* dev.clipall is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* You should have received a copy of the GNU General Public License
-* along with dev.clipall.  If not, see <http://www.gnu.org/licenses/>.
-*
-* File      : GenericMediator.java
-* Created   : Nov 2009
-*/
-
+ * This file is part of dev.clipall.
+ *
+ * dev.clipall is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dev.clipall.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * File      : GenericMediator.java
+ * Created   : Nov 2009
+ */
 package dev.clipall.business;
 
 import dev.clipall.Constants;
@@ -25,11 +24,13 @@ import dev.clipall.utils.Utils;
 import dev.clipall.view.SearchFrame;
 import dev.clipall.view.SearchPanel;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.util.List;
 import dev.utils.collections.Reversed;
-import java.util.ArrayList;
+import dev.utils.io.FileHelper;
+import dev.utils.log.Logger;
 import java.util.Arrays;
 
 /**
@@ -37,43 +38,42 @@ import java.util.Arrays;
  * @author erol
  */
 public class GenericMediator extends Constants {
-    
+
     private static GenericMediator mediator = new GenericMediator();
-    
-    private GenericMediator(){}
-    
-    public static GenericMediator getInstance(){
+
+    private GenericMediator() {
+    }
+
+    public static GenericMediator getInstance() {
         return mediator;
     }
-    
+
     //------------------------------------------------------
-    
-    public void setNextCategoryEvent(){
-        SearchPanel.getInstance().updateSearchPanelFields();
-    }
-    
-    public void setPreviousCategoryEvent(){
+    public void setNextCategoryEvent() {
         SearchPanel.getInstance().updateSearchPanelFields();
     }
 
-    public void setCurrentCategoryEvent(){
+    public void setPreviousCategoryEvent() {
         SearchPanel.getInstance().updateSearchPanelFields();
-    }   
+    }
 
-    public void itemsToClipboardEvent(List<File> list){
+    public void setCurrentCategoryEvent() {
+        SearchPanel.getInstance().updateSearchPanelFields();
+    }
 
-        GenericModel.getInstance().getCurrentCategory()
-                                        .addItem("", Utils.filesToString(list), FlavorTypes.FILE);
+    public void itemsToClipboardEvent(List<File> list) {
+
+        GenericModel.getInstance().getCurrentCategory().addItem("", Utils.filesToString(list), FlavorTypes.FILE);
 
         SearchPanel.getInstance().updateJSearchList();
     }
 
-    public Item itemsToClipboardEvent(String item){
+    public Item itemsToClipboardEvent(String item) {
         Item itemObj = new Item("", item, FlavorTypes.STRING);
         return itemsToClipboardEvent(itemObj);
     }
 
-    public Item itemsToClipboardEvent(Item item){
+    public Item itemsToClipboardEvent(Item item) {
         ExtendedItem exItem = new ExtendedItem(item);
         exItem.setCategory(GenericModel.getInstance().getCurrentCategory());
         GenericModel.getInstance().getCurrentCategory().addItem(exItem);
@@ -81,28 +81,29 @@ public class GenericMediator extends Constants {
         return exItem;
     }
 
-    public void deleteItemEvent(Item item){
+    public void deleteItemEvent(Item item) {
         //GenericModel.getInstance().getCurrentCategory().removeItem(item);
         ExtendedItem exItem = (ExtendedItem) item;
         exItem.getCategory().removeItem(exItem);
         SearchPanel.getInstance().updateJSearchList();
     }
 
-    
     public void enterPressedForOneSelectedItem(String searchedItem) {
 
         Item selectedItem = SearchPanel.getInstance().getSelectedItem();
 
-        if(selectedItem == null){ return; }
+        if (selectedItem == null) {
+            return;
+        }
 
         itemsToClipboardEvent(selectedItem);
 
         ClipboardLogic.getInstance().setClipboard(selectedItem);
-        
+
         Platform.getInstance().paste();
     }
 
-    public void enterPressedOnSearchListEvent(String searchedItem){
+    public void enterPressedOnSearchListEvent(String searchedItem) {
         enterPressedOnSearchListEvent(searchedItem, false);
     }
 
@@ -110,13 +111,15 @@ public class GenericMediator extends Constants {
 
         Item[] items = SearchPanel.getInstance().getSelectedItems();
 
-        if(items.length == 0){ return; }
+        if (items.length == 0) {
+            return;
+        }
 
-        if(items.length == 1){
+        if (items.length == 1) {
             enterPressedForOneSelectedItem(searchedItem);
             return;
         }
-        
+
         //Item item = itemsToClipboardEvent(itemsToString(items));
         Item item = new Item("", itemsToString(items, reverseOrder), FlavorTypes.STRING);
 
@@ -125,17 +128,17 @@ public class GenericMediator extends Constants {
         Platform.getInstance().paste();
     }
 
-    public String itemsToString(Item[] items, boolean reverseOrder){
+    public String itemsToString(Item[] items, boolean reverseOrder) {
 
         StringWriter sw = new StringWriter();
         PrintWriter pw = new PrintWriter(sw);
 
-        if(reverseOrder){
-            for(Item item : Reversed.reversed(Arrays.asList(items))){
+        if (reverseOrder) {
+            for (Item item : Reversed.reversed(Arrays.asList(items))) {
                 pw.println(item.getItem());
             }
         } else {
-            for(Item item : items){
+            for (Item item : items) {
                 pw.println(item.getItem());
             }
         }
@@ -155,7 +158,7 @@ public class GenericMediator extends Constants {
         SearchPanel.getInstance().updateSearchPanelFields();
     }
 
-    public void deleteCurrentCategory(){
+    public void deleteCurrentCategory() {
 
         Category category = GenericModel.getInstance().getCurrentCategory();
         GenericModel.getInstance().getCategories().deleteCategory(category);
@@ -166,17 +169,66 @@ public class GenericMediator extends Constants {
         SearchPanel.getInstance().updateSearchPanelFields();
     }
 
-    public void displaySearchPanel(){
+    public void displaySearchPanel() {
         //SearchPanel.getInstance().focusOnSearchTextField();
         SearchFrame.getInstance().displaySearchFrame();
     }
 
     public List<Item> getSearchListItems(String string) {
-        
-        if(SearchPanel.getInstance().isAllCategoriesSearch()){
+
+        if (SearchPanel.getInstance().isAllCategoriesSearch()) {
             return SearchItemLogic.getInstance().queryAllCategories(string);
         } else {
             return SearchItemLogic.getInstance().queryTheCurrentCategory(string);
-        }        
+        }
+    }
+
+    // TortoiseProc.exe /command:diff /path:"c:/mehmet_tel" /path2:"c:/kyk_ode"
+    public void compareItems() {
+
+        Item[] items = SearchPanel.getInstance().getSelectedItems();
+
+        if (items.length != 2) {
+            return;
+        }
+
+        String file1 = GenericLogic.getInstance().getTmpFile("f1");
+        String file2 = GenericLogic.getInstance().getTmpFile("f2");
+
+        FileHelper.createFile(file1, items[0].getItem());
+        FileHelper.createFile(file2, items[1].getItem());
+
+        String command = "cmd /c TortoiseProc.exe /command:diff" +
+                " /path:" + "\"" + file1 + "\"" +
+                " /path2:" + "\"" + file2 + "\"";
+        try {
+            Runtime.getRuntime().exec(command);
+        } catch (IOException ex) {
+            Logger.getLogger().error("", ex, getClass());
+        }
+    }
+
+    public void compareItemsWithDiffMerge() {
+
+        Item[] items = SearchPanel.getInstance().getSelectedItems();
+
+        if (items.length != 2) {
+            return;
+        }
+
+        String file1 = GenericLogic.getInstance().getTmpFile("f1");
+        String file2 = GenericLogic.getInstance().getTmpFile("f2");
+
+        FileHelper.createFile(file1, items[0].getItem());
+        FileHelper.createFile(file2, items[1].getItem());
+
+        String command = "cmd /c diffmerge.exe" +
+                " \"" + file1 + "\"" +
+                " \"" + file2 + "\"";
+        try {
+            Runtime.getRuntime().exec(command);
+        } catch (IOException ex) {
+            Logger.getLogger().error("", ex, getClass());
+        }
     }
 }
